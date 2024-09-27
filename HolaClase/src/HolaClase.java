680d50b140b798052b5418dbf48cdd8f79ba0a17
Samuel <samuelayllonsevilla86@gmail.com>
2024-09-27 12:55:35 +0200

    Add files via upload

---
+++ HolaClase/src/HolaClase.java
@@ -0,0 +1,17 @@
+public class HolaClase {
+    public static void main(String[] args) throws Exception {
+        
+        System.out.println("Hola Alumnos!");
+        
+        //Podemos añadir espacios usando comandos como : \n(space), \t(tab)
+        System.out.println("Hola" + "\nAdiós");
+        System.out.println("Hola" + "\tAdiós");
+
+        //Tipos de datos primitivos
+        byte b = 1
+        short s = 1;
+
+         
+
+    }
+}
