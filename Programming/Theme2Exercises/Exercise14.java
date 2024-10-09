2c165eb6a815a2f4f3582aa8b0f9442592cfb53a
Samuel <samuelayllonsevilla86@gmail.com>
2024-10-09 10:39:48 +0200

    Things

---
+++ Programming/Theme2Exercises/Exercise14.java
@@ -0,0 +1,21 @@
+package Programming.Theme2Exercises;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Exercise14 {
+    public static void main(String[] args) {
+
+        //Instance block
+        Scanner input = new Scanner(System.in);
+        String message;
+        int number,randomNumber, counter;
+
+        //User data
+        System.out.println("WELCOME to the enigma game \n You have 5 opportunities to can guess the number");
+        number = input.nextInt();
+
+        //Instructions
+        randomNumber = (int) Math.random();
+    }
+}
