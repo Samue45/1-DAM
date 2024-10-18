26e48b7b08e161957949044e7262d52484d08bcf
Samuel <samuelayllonsevilla86@gmail.com>
2024-10-18 11:10:43 +0200

    Project

---
+++ Programming/Theme2/Project/Math/src/Birthday.java
@@ -0,0 +1,7 @@
+public class Birthday {
+
+    public void congratulate(String name, int age){
+
+        System.out.println("Happy birthday " + name + ", now you are " + age + " old :)");
+    }
+}
---
+++ Programming/Theme2/Project/Math/src/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        Birthday felicitation = new Birthday();
+
+        felicitation.congratulate("Samuel",19);
+    }
+}
