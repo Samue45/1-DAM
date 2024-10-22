d901ed3fa21fcd8a63023422ed3c6e8457bfa0f6
Samuel <samuelayllonsevilla86@gmail.com>
2024-10-22 13:18:04 +0200

    Factorization

--- Programming/Theme2/Project/Math/src/Main.java
+++ Programming/Theme2/Project/Math/src/Main.java
@@ -2,7 +2,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println(Maths.incrementOne(3));
-        System.out.println(Maths.divisor(10,2));
+
+        System.out.println(Maths.factorization(20));
     }
 }
--- Programming/Theme2/Project/Math/src/Maths.java
+++ Programming/Theme2/Project/Math/src/Maths.java
@@ -1,9 +1,5 @@
 public class Maths {
 
-    public static String congratulate(String name, int age){
-        return "Happy birthday " + name + ", now you are " + age + " old :)";
-
-    }
 
     public static double potency(double base, double exponent){
        //Block instance
@@ -29,9 +25,51 @@ public class Maths {
 
     public static  int incrementOne(int number){
         return ++number;
-    };
+    }
 
     public static boolean divisor(int number1, int number2){
         return (number1 % number2) ==0;
+    }
+
+    public static String allDivisor(int number){
+        String divisor= "1";
+
+        for (int i = 2; i <= number;i++){
+            if (divisor(number,i)) divisor = divisor + "-" + i;
+        }
+        return divisor;
+    }
+
+
+    public static int getFirstPrimeFactor(int num) {
+        // Check for the smallest prime factor
+        for (int cont = 2; cont <= num; cont++) {
+            if (num % cont == 0) { // Check if cont is a divisor
+                return cont; // Return the first prime factor
+            }
+        }
+        return num; // Return the number itself if it's prime
+    }
+
+    public static String factorization(int number) {
+        if (number < 2) {
+            return "Number must be greater than 1.";
+        }
+
+        String factorization = "";
+
+        while (number > 1) {
+            int factor = getFirstPrimeFactor(number);
+            number /= factor;
+
+            factorization += factor + "*"; // Concatenate the factor
+        }
+
+        // Remove the last '*' if there's any factorization
+        if (factorization.length() > 0) {
+            factorization = factorization.substring(0, factorization.length() - 1); // Remove last '*'
+        }
+
+        return factorization;
     }
 }
