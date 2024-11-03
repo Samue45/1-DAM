67cadf49991dddfb2e1f30dd549c27125bb89a6c
Samuel <136899093+Samue45@users.noreply.github.com>
2024-11-03 12:22:50 +0100

    new

---
+++ Markup language/JS/Practise1/script.js
@@ -0,0 +1,12 @@
+//Input
+let solution = document.getElementById('resultado');
+
+//I need to do this:
+/*
+1º Look for all HTMLELMENTS in the DOOM
+2º Set Listener to all buttons
+3º Give a function to each button
+4º Set special function to the button '=', 'C'
+*/ 
+
+
