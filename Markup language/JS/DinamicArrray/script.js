59820245d7ab795ef2b3cc8505256ac8ba96e0ea
Samuel <136899093+Samue45@users.noreply.github.com>
2024-11-19 16:21:17 +0100

    Array

--- Markup language/JS/DinamicArrray/script.js
+++ Markup language/JS/DinamicArrray/script.js
@@ -1,5 +1,5 @@
 //Properties
-let input, list;
+let input, list, error;
 let arrayNumbers = new Array();
 
 
@@ -8,23 +8,39 @@ function main(){
     //Look for the elements in the DOM
     input = document.getElementById('input');
     list = document.getElementById('list');
+    error = document.getElementById('error');
     document.getElementById('button').addEventListener('click', addNumber)
 }
 
 function addNumber(){
-    // Add the number to the Array
-    arrayNumbers.push(parseInt(input.value));
+    
+    if(validateData() == true){
+        //We show an error message
+        error.innerHTML = `ERROR :The value ${input.value} isn't a number :(`;
+    }else{
+        //We delete the error message
+        error.innerHTML = ``;
 
-    //We walk inside an Array
-    let cadena = '';
-    for(let i = 0; i < arrayNumbers.length; i++){
+        // Add the number to the Array
+        arrayNumbers.push(parseInt(input.value));
+
+        //We walk inside an Array and save the number in a String
+        let cadena = '';
+        for(let i = 0; i < arrayNumbers.length; i++){
         //Add the number to a Srtring list
         cadena += `<li>Number : ${arrayNumbers[i]}</li>`
+        }
+
+        //Show the list of all numbers
+        list.innerHTML = cadena;
     }
+    
 
-    //Show the list of all numbers
-    list.innerHTML = cadena;
+}
 
+//Function to check if user data is a number or not
+function validateData(){
+    return isNaN(parseInt(input.value));
 }
 
 
