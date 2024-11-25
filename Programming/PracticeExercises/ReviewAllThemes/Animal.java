66bfd7d8e34931c5cd191608bb39effd8005fadf
Samuel <samuelayllonsevilla86@gmail.com>
2024-11-25 09:41:43 +0100

    Exercise 1

---
+++ Programming/PracticeExercises/ReviewAllThemes/Animal.java
@@ -0,0 +1,12 @@
+package Programming.PracticeExercises.ReviewAllThemes;
+
+public class Animal {
+
+
+    public Animal(){}
+
+
+    public void doSong() {
+        System.out.println("The animal do a song.");
+    }
+}
---
+++ Programming/PracticeExercises/ReviewAllThemes/Dog.java
@@ -0,0 +1,11 @@
+package Programming.PracticeExercises.ReviewAllThemes;
+
+public class Dog extends Animal{
+
+    public Dog(){}
+
+    @Override
+    public void doSong() {
+        System.out.println("The dog does: Guau, Guau!.");
+    }
+}
