8ce76824ac0a47d68c18ffd74fa4db8ef7cb092f
Samuel <136899093+Samue45@users.noreply.github.com>
2024-11-26 18:01:39 +0100

    Exercise 1

--- Programming/PracticeExercises/Exam/Main.java
+++ Programming/PracticeExercises/Exam/Main.java
@@ -1,11 +1,11 @@
 package Programming.PracticeExercises.Exam;
 
-import java.util.Arrays;
+
 
 public class Main {
 
     public static void main(String[] arg){
-       Person person = new Person();
-       person.estandarPersona();
+       JuegoDado juegoDado = new JuegoDado();
+       juegoDado.jugar();
     }
 }
