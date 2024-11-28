b0f64b93445f30d327905d34aa531e95eb61fac0
Samuel <samuelayllonsevilla86@gmail.com>
2024-11-28 13:56:28 +0100

    Exercise 4

---
+++ Markup language/JS/Exercise4/script.js
@@ -0,0 +1,140 @@
+let categories = new Array();
+let limpieza = new Array();
+let lacteos = new Array();
+let reposteria = new Array();
+let vegetales = new Array();
+let carnes = new Array();
+
+//Productos 
+//Producto de la categoría limpieza
+limpieza = [
+    {
+        balletas: {
+            marca : 'Ball',
+            precio : 2.40,
+            descripcion : 'Las mejores balletas para limpiar'
+
+        },
+        friegaSuelos: {
+            marca : 'CleanFloor',
+            precio : 3.40,
+            descripcion : 'Deja tu suelo como los choros de oro'
+        },
+        suavizante: {
+            marca : 'FreshClothe',
+            precio : 4.00,
+            descripcion : 'Dejrá tu ropa fresca y con olor a rosas'
+        }
+    }
+]
+ //Producto de la categoría lacteos
+ lacteos = [
+    {
+        balletas: {
+            marca : 'Ball',
+            precio : 2.40,
+            descripcion : 'Las mejores balletas para limpiar'
+
+        },
+        friegaSuelos: {
+            marca : 'CleanFloor',
+            precio : 3.40,
+            descripcion : 'Deja tu suelo como los choros de oro'
+        },
+        suavizante: {
+            marca : 'FreshClothe',
+            precio : 4.00,
+            descripcion : 'Dejrá tu ropa fresca y con olor a rosas'
+        }
+    }
+]
+ //Producto de la categoría reposteria
+ reposteria = [
+    {
+        balletas: {
+            marca : 'Ball',
+            precio : 2.40,
+            descripcion : 'Las mejores balletas para limpiar'
+
+        },
+        friegaSuelos: {
+            marca : 'CleanFloor',
+            precio : 3.40,
+            descripcion : 'Deja tu suelo como los choros de oro'
+        },
+        suavizante: {
+            marca : 'FreshClothe',
+            precio : 4.00,
+            descripcion : 'Dejrá tu ropa fresca y con olor a rosas'
+        }
+    }
+]
+ //Producto de la categoría vegetales
+ vegetales = [
+    {
+        balletas: {
+            marca : 'Ball',
+            precio : 2.40,
+            descripcion : 'Las mejores balletas para limpiar'
+
+        },
+        friegaSuelos: {
+            marca : 'CleanFloor',
+            precio : 3.40,
+            descripcion : 'Deja tu suelo como los choros de oro'
+        },
+        suavizante: {
+            marca : 'FreshClothe',
+            precio : 4.00,
+            descripcion : 'Dejrá tu ropa fresca y con olor a rosas'
+        }
+    }
+]
+ //Producto de la categoría carnes
+ carnes = [
+    {
+        balletas: {
+            marca : 'Ball',
+            precio : 2.40,
+            descripcion : 'Las mejores balletas para limpiar'
+
+        },
+        friegaSuelos: {
+            marca : 'CleanFloor',
+            precio : 3.40,
+            descripcion : 'Deja tu suelo como los choros de oro'
+        },
+        suavizante: {
+            marca : 'FreshClothe',
+            precio : 4.00,
+            descripcion : 'Dejrá tu ropa fresca y con olor a rosas'
+        }
+    }
+]
+
+categories = [
+    limpieza,
+    lacteos,
+    reposteria,
+    vegetales,
+    carnes
+]
+
+
+
+function main(){
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+window.addEventListener('load',main);
