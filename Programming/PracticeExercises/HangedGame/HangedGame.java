b59ff4eee984d7f37c617e4078d170404f90f4d7
Samuel <samuelayllonsevilla86@gmail.com>
2024-11-29 14:40:43 +0100

    HangedGame

--- Programming/PracticeExercises/HangedGame/HangedGame.java
+++ Programming/PracticeExercises/HangedGame/HangedGame.java
@@ -20,7 +20,8 @@ public class HangedGame {
 
     }
 
-    public String randomWord(){
+    //Generate a random word to can play
+    private String randomWord(){
         String word;
         int randNumber = random.nextInt(0,4);
 
@@ -29,7 +30,8 @@ public class HangedGame {
         return word;
     }
 
-    public char answerUser(){
+    //Ask user a letter
+    private char answerUser(){
         String  answer;
         char letter;
         //We ask a word
@@ -41,7 +43,39 @@ public class HangedGame {
         return letter;
     }
 
-    public boolean compareValues(String word, char letter){
-        return false;
+    //Check if the random word have the letter that user give us
+    private boolean compareValues(String word, char letter){
+        boolean result = false;
+
+        for (int i = 0; i < word.length() ; i++) {
+            if (word.charAt(i) == letter){
+                result = true;
+            }
+
+        }
+
+        return result;
+    }
+
+    //Check if the user already say the letter
+    private boolean checkLetter(char letter){
+        boolean result = false;
+        char[] abecedario = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+
+        for (int i = 0; i < abecedario.length ; i++) {
+            if (abecedario[i] == letter) {
+                result = true;
+            }
+        }
+        return  result;
+    }
+
+
+    private void saveAnswer(String word,char letter){
+        char[] answer = new char[word.length()];
+
+        for (int i = 0; i < answer.length; i++) {
+
+        }
     }
 }
