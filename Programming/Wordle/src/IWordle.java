17d8731fcf4fb7b9eff2001ec761d2ed40e98297
Samuel <samuelayllonsevilla86@gmail.com>
2024-12-10 13:30:30 +0100

    Wordle

---
+++ Programming/Wordle/src/IWordle.java
@@ -0,0 +1,15 @@
+public interface IWordle {
+
+    public void wordle(); // m�todo principal d�nde se juega
+    public String generarPalabraSecreta();// obtiene una palabra aleatoria
+    public TiposWordle letraPertenece(String palabraSecreta, char letra, int pos);
+    // Retorno si est� dentro la letra y si est� en la posici�n
+
+    public TiposWordle[] getAciertos(String palabraSecreta, String palabra);
+    // Retorno todos los aciertos de la palabra introducido
+
+    public boolean acertado( TiposWordle[] aciertos);
+    // Retorna true cuando todas letras est�n a ESTA_DENTRO
+
+    public void printlnColores(String palabra,TiposWordle[] aciertos);
+}
