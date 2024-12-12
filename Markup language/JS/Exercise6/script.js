18061895947f4f7286089e7ca4fb80d6c1d00fa6
Samuel <136899093+Samue45@users.noreply.github.com>
2024-12-12 19:03:34 +0100

    Exercise 6

--- Markup language/JS/Exercise6/script.js
+++ Markup language/JS/Exercise6/script.js
@@ -1,44 +1,48 @@
-let inputNumber, list;
+let inputNumber, container;
 
 function main(){
 
     inputNumber = document.getElementById('inputNumber');
-    list = document.getElementById('list');
+    container = document.getElementById('container');
 
     document.getElementById('button').addEventListener('click', primeNumbers);
 }
 
 function primeNumbers(){
     //1º Create an Array to can calculate the primes numbers and set a style and the begining of our list
-    let allNumbers =[parseInt(inputNumber.value) ];
+    let allNumbers = new Array(parseInt(inputNumber.value));
     //The list
-    let finalSolution = '';
+    let list = `<ul>`;
 
     //2º We run the Array to calculate what numbers are primes and also to set a style
-    for (let index = 0; index < allNumbers.length; index++) {
-        //Number
-        let primeNumber;
+    for (let i = 0; i < allNumbers.length; i++) {
+    
+        //Save the numbers inside
+        allNumbers[i] = i + 1;
         //Check if the number is prime
-        if((allNumbers[index] % (1)) == 0 && (allNumbers[index] % (allNumbers.length)) == 0) {
-            primeNumber = allNumbers[index];
+        if(isPrime(allNumbers[i])){
+            list += `<li> ${allNumbers[i]} </li>`;
         }
-
-       /*  //Check if it is even to set a style
-        if((allNumbers[index] % 2) == 0){
-            primeNumber.classList.add("red-decoration");
-        }else{
-            primeNumber.classList.add("blue-decoration");
-        }
-        */
-
-        //Finally we save the number on the list
-        finalSolution += `<li>${primeNumber}</li>`
-        
     }
-
     //Close the list
-    list.innerHTML = finalSolution;
+    list += `</ul>`;
+
+    container.innerHTML = list;
 }
+
+
+//Check if the number is prime
+function isPrime(number){
+    if (number < 2) return false; // Numbers less than 2 are not prime
+    for (let i = 2; i <= Math.sqrt(number); i++) {
+        if (number % i === 0) {
+            return false; // Number is divisible by another number, so it's not prime
+        }
+    }
+    return true; // If no divisors are found, the number is prime
+}
+
+//Create a method to change into the style of the <li> depends on if it's even or odd
 
 window.addEventListener('load', main);
 
