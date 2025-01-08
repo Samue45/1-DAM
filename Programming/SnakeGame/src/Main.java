10f4ed96a4d86748dc71edb600af6f2fb5cf1f79
Samuel <136899093+Samue45@users.noreply.github.com>
2025-01-08 16:21:02 +0100

    Adding the snake to the table and change into the class Snake

--- Programming/SnakeGame/src/Main.java
+++ Programming/SnakeGame/src/Main.java
@@ -8,8 +8,8 @@ public class Main {
     public static void main(String[] args) {
 
         Apple apple = new Apple(16,16, Color.RED);
-        Snake snake = new Snake(20,20, Color.GREEN);
 
-        PrincipalScreen.createWindow(apple,snake);
+
+        //PrincipalScreen.createWindow(apple,snake);
     }
 }
--- Programming/SnakeGame/src/Snake/Snake.java
+++ Programming/SnakeGame/src/Snake/Snake.java
@@ -1,78 +1,87 @@
 package Snake;
-
 import java.awt.*;
 import java.util.LinkedList;
 
 public class Snake {
 
     //Properties
-    private LinkedList<int[]> body;
-    private int positionX;
-    private int positionY;
-    private Color color;
-
-
+    private LinkedList<Body > body;
+    private String direction;
 
 
     //Constructor
-    public Snake(int startX, int startY) {
+    //We inicializate our Snake creating its head
+    public Snake(Body newBody) {
         body = new LinkedList<>();
-        body.add(new int[] { startX, startY }); // Cabeza de la serpiente
+        body.add(newBody); // Snake head
     }
 
-    public Snake(int positionX, int positionY, Color color) {
-        this.positionX = positionX;
-        this.positionY = positionY;
-        this.color = color;
-    }
 
-    public Snake() {}
+    public Snake() {
+        body = new LinkedList<>();
+    }
 
     //Getter and Setter
-    public LinkedList<int[]> getBody() {
+    public LinkedList<Body> getBody() {
         return body;}
 
-    public int getPositionX() {
-        return positionX;
-    }
-
-    public void setPositionX(int positionX) {
-        this.positionX = positionX;
-    }
-
-    public int getPositionY() {
-        return positionY;
-    }
-
-    public void setPositionY(int positionY) {
-        this.positionY = positionY;
-    }
 
     //Methods of Snake class
-   // The snake increase its size when eats the apple
-    /*
-    public void mover(int nuevaX, int nuevaY) {
-            body.addFirst(new int[] { nuevaX, nuevaY }); // Añade nueva posición como cabeza
+    public void mover(int newX, int newY) {
+            Body newBody = new Body(newX,newY,Color.green);
+            body.addFirst(newBody); // Añade nueva posición como cabeza
             body.removeLast(); // Quita la última posición (simula movimiento)
     }
 
-    public void crecer() {
-            // No elimines la última posición (simula el crecimiento de la serpiente)
-            body.addLast(body.getLast());
-    }*/
+    public void grown() {
+        Body last = body.getLast(); // Obtiene el último segmento
+        int newX = last.getPositionX();
+        int newY = last.getPositionY();
 
+        // Determina la posición del nuevo segmento según la dirección
+        switch (direction.toUpperCase()) {
+            case "UP":
+                newY += 1; // Crece hacia abajo respecto al último segmento
+                break;
+            case "DOWN":
+                newY -= 1; // Crece hacia arriba respecto al último segmento
+                break;
+            case "LEFT":
+                newX += 1; // Crece hacia la derecha respecto al último segmento
+                break;
+            case "RIGHT":
+                newX -= 1; // Crece hacia la izquierda respecto al último segmento
+                break;
+        }
 
-    //The snake dead when its head crashes with the limit of the map or with its body
-    //Its head is the first column of our matrix, if the head position is equal to another like his body or is bigger than limits of the map
-    // it means that it's dead
-   /* public boolean dead(Snake snake){
+        // Create and add the new segment
+        Body newSegment = new Body(newX, newY, last.getColor());
+        body.addLast(newSegment);
+    }
+
+    public boolean dead(){
+
         boolean isDead = false;
 
-        //Position of its head
+        //Positions of its head
+        Body head = body.getFirst();
+        int headX = head.getPositionX();
+        int headY = head.getPositionY();
 
+        //If Head snake has the same position that some part of its body -> Dead
+        //1º We need to run the Snake body and compare all the positions with its head position
+        for (int i = 1; i < body.size(); i++) {
+            Body segment = body.get(i);
+            if (headX == segment.getPositionX() && headY == segment.getPositionY()){
+                isDead = true;
+            }
+        }
 
-        //if (snake.getPositionX() == getPositionY() || ){}
+        //If Head snake has a position bigger or littler than the map -> Dead
+        if (headX > 35 || headX < 0 ){
+            isDead = true;
+        }
 
         return isDead;
-    }*/
+    }
 }
