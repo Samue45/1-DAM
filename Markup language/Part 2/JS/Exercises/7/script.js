f3c06bd624ca284c87654a3835d5c8f3c58f9150
Samuel <samuelayllonsevilla86@gmail.com>
2025-01-16 10:16:53 +0100

    Updates

--- Markup language/Part 2/JS/Exercises/7/script.js
+++ Markup language/Part 2/JS/Exercises/7/script.js
@@ -1,60 +1,75 @@
 //Properties
-let table;
-let studentArray = new Array();
+
+//Create headers for the table
+let headers = ["Students","Note 1","Note 2","Note 3","Media"];
+    
+//Create the literal objects
+let studentArray =[{
+    name : "Paco",
+    note1 : 8,
+    note2 : 7,
+    note3: 9   
+},
+{
+    name : "María",
+    note1 : 10,
+    note2 : 9,
+    note3: 9   
+},
+{
+    name : "Antonio",
+    note1 : 5,
+    note2 : 7,
+    note3: 6   
+},
+//Calcular media como un objeto literal
+media =>{
+
+}]; 
+
 
 
 function main(){
-    //Look for the elements in the DOM
-    table = document.createElement('table');
     document.getElementById('button').addEventListener('click', createTable);
     
-
-    //Create headers for the table
-    headers = ["Students","Note 1","Note 2","Note 3","Media"];
-    
-    //Create the literal objects
-    studentArray =[student1 = {
-        name : "Paco",
-        note1 : 8,
-        note2 : 7,
-        note3: 9   
-    },
-    student2 = {
-        name : "María",
-        note1 : 10,
-        note2 : 9,
-        note3: 9   
-    },
-    student3 = {
-        name : "Antonio",
-        note1 : 5,
-        note2 : 7,
-        note3: 6   
-    }]; 
-        
 }
 
 //Method to create the student table
 function createTable(){
-
+    
     //Create the header of the table
-    let tr1 = document.createElement('tr');
+
+    let table = document.createElement('table');
+
+    let trHeader = document.createElement('tr');
     for(let i= 0; i < headers.length; i++){
         // We create the elements th and add to the element tr
         let th = document.createElement('th');
-        th.textContent = headers[i];
-        tr1.appendChild(th);
+        // 1º Opción = th.textContent = headers[i];
+        //2º Opción
+        // let texto = document.createTextNode(headers[i]);
+        th.appendChild(document.createTextNode(headers[i]));
+        trHeader.appendChild(th);
     }
     // We add the header to the table
-    table.appendChild(tr1);
+    table.appendChild(trHeader);
 
     //Create rest of the table
-    let media = 0;
-    let tr2;
     for(let i=0; i < studentArray.length; i++){
-        tr2 = document.createElement('tr');
+        let media = 0;
+
+        let trAlumno = document.createElement('tr');
         //Calculate the media of each student
         media =(studentArray[i].note1 + studentArray[i].note2 + studentArray[i].note2) / 3;
+
+        //Crear las columnas de cada fila usando un bucle for
+        for (let index = 0; index < studentArray.length; index++) {
+            let td = document.createElement('td');
+            td.appendChild(studentArray[i]);
+            
+            
+            trAlumno.appendChild(td);
+        }
     
         //Create the rest of the table and concatenate with the information variable .
         let td1 = document.createElement('td');
@@ -69,19 +84,18 @@ function createTable(){
         td4.textContent = studentArray[i].note3;
         td5.textContent = media;
 
-        tr2.appendChild(td1);
-        tr2.appendChild(td2);
-        tr2.appendChild(td3);
-        tr2.appendChild(td4);
-        tr2.appendChild(td5);
+        trAlumno.appendChild(td1);
+        trAlumno.appendChild(td2);
+        trAlumno.appendChild(td3);
+        trAlumno.appendChild(td4);
+        trAlumno.appendChild(td5);
 
         //Save all the information inside on the table
-        table.appendChild(tr2);
+        table.appendChild(trAlumno);
 
     }
-
     //Finally add the table to the body
-    document.body.appendChild(table);
+    document.body.appendChild(table); 
 }
 
 
