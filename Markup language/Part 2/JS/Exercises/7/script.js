e909a2422f57b4fb6faa8f87b5a391b898a6abd8
Samuel <136899093+Samue45@users.noreply.github.com>
2025-01-16 17:14:40 +0100

    changesEjercicio7

--- Markup language/Part 2/JS/Exercises/7/script.js
+++ Markup language/Part 2/JS/Exercises/7/script.js
@@ -21,10 +21,6 @@ let studentArray =[{
     note1 : 5,
     note2 : 7,
     note3: 6   
-},
-//Calcular media como un objeto literal
-media =>{
-
 }]; 
 
 
@@ -42,60 +38,51 @@ function createTable(){
     let table = document.createElement('table');
 
     let trHeader = document.createElement('tr');
-    for(let i= 0; i < headers.length; i++){
+    headers.forEach((element =>{
         // We create the elements th and add to the element tr
         let th = document.createElement('th');
-        // 1º Opción = th.textContent = headers[i];
-        //2º Opción
-        // let texto = document.createTextNode(headers[i]);
-        th.appendChild(document.createTextNode(headers[i]));
+        th.textContent = element;
         trHeader.appendChild(th);
-    }
+    }))
+
     // We add the header to the table
     table.appendChild(trHeader);
 
     //Create rest of the table
-    for(let i=0; i < studentArray.length; i++){
-        let media = 0;
-
+    studentArray.forEach((element => {
+        //Creamos la fila de la tabla
         let trAlumno = document.createElement('tr');
-        //Calculate the media of each student
-        media =(studentArray[i].note1 + studentArray[i].note2 + studentArray[i].note2) / 3;
 
-        //Crear las columnas de cada fila usando un bucle for
-        for (let index = 0; index < studentArray.length; index++) {
+        //Creamos cada columna de la tabla y le asignamos un valor a cada una 
+        //El método Object.keys() me permite recorrer las propiedades de un objeto literal como si fuera un Array
+        Object.keys(element).forEach((propiedad => {
+
             let td = document.createElement('td');
-            td.appendChild(studentArray[i]);
+            td.textContent = element[propiedad]; // Así accedo al valor de cada propiedad del objeto literal
+
+            //Falta calcular la media 
             
-            
+            //Guardamos la columna en la fila
             trAlumno.appendChild(td);
-        }
+        }))
     
-        //Create the rest of the table and concatenate with the information variable .
-        let td1 = document.createElement('td');
-        let td2 = document.createElement('td');
-        let td3 = document.createElement('td');
-        let td4 = document.createElement('td');
-        let td5 = document.createElement('td');
 
-        td1.textContent = studentArray[i].name;
-        td2.textContent = studentArray[i].note1;
-        td3.textContent = studentArray[i].note2;
-        td4.textContent = studentArray[i].note3;
-        td5.textContent = media;
+         //Finalmente guardamos la fila en la tabla
+          table.appendChild(trAlumno);
 
-        trAlumno.appendChild(td1);
-        trAlumno.appendChild(td2);
-        trAlumno.appendChild(td3);
-        trAlumno.appendChild(td4);
-        trAlumno.appendChild(td5);
-
-        //Save all the information inside on the table
-        table.appendChild(trAlumno);
-
-    }
+    }))
     //Finally add the table to the body
     document.body.appendChild(table); 
+}
+
+function media(){
+    let media = 0;
+
+    studentArray.forEach((element => {
+        media = (element.note1 + element.note2 + element.note3) / 3;
+    }))
+
+    return media;
 }
 
 
