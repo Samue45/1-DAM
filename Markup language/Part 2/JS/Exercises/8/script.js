cca190e5d15f4b037676c58d2b4581b7c655c9c4
Samuel <136899093+Samue45@users.noreply.github.com>
2025-01-19 13:54:46 +0100

    Ejercicio8

--- Markup language/Part 2/JS/Exercises/8/script.js
+++ Markup language/Part 2/JS/Exercises/8/script.js
@@ -1,23 +1,117 @@
 
 window.addEventListener('load', main);
 
+let arrayTareas = [];
+let table;
+
 function main(){
+    
+    //Botón
+    document.querySelector('button').addEventListener('click', createTask);
 
-    document.querySelector("button").addEventListener('click', viajarForm);
-    document.getElementById("create-task").addEventListener('click', viajarHome);
-    document.getElementById("cancel-task").addEventListener('click', viajarHome);
+    //Tabla
+    table = document.createElement('table');
+    //Creamos la cabecera de la tabla y la añadimos a la tabla
+    crearCabecera();
+
+    //Mostramos la tabla
+    mostrarTareas();
+}
+
+function createTask(){
+    
+    //1º Agarrar los inputs del DOM y obtener sus valores
+    let nombre = document.getElementById('nombre').value;
+    let descripcion = document.getElementById('descripcion').value;
+    let fecha = document.getElementById('fecha').value;
+
+
+    //2º Crear un objeto literal con los datos obtenidos
+    let task = {
+        nombre : nombre,
+        descripcion: descripcion,
+        fecha : fecha,
+        estado : "No completada",
+        button: null
+    }
+
+    //3º Guardar los datos en el arrayTareas
+    arrayTareas.push(task);
+
+    //4º Creamos las filas y las añadimos a la tabla
+    crearFilas();
+}
+
+function mostrarTareas(){
+
+    //Añadimos la tabla con todos sus elementos al DOM
+    document.body.appendChild(table);
+}
+
+function crearCabecera(){
+
+    let tablaHeaders = ['Nombre', 'Descripción', 'Fecha', 'Estado'];
+
+    //1º Añadir encabezado a la tabla
+    let trHeader = document.createElement('tr');
+    tablaHeaders.forEach((element =>{
+        let thHeader = document.createElement('th');
+        thHeader.textContent = element;
+        trHeader.appendChild(thHeader);
+    }))
+
+    table.appendChild(trHeader);
 
 }
 
+function crearFilas(){
+    
+    arrayTareas.forEach((element => {
 
+        //1º Por cada objeto literal creamos una fila
+        let trTask = document.createElement('tr');
 
+        //2º Por cada propiedad del objeto literal creamos una columna
+        crearColumnas(trTask,element);
 
-
-//Funciones para viajar entre páginas
-function viajarForm(){
-    window.location.href = "form.html";
+        //3º Añadimos la fila a la tabla
+        table.appendChild(trTask);
+     }))
 }
 
-function viajarHome(){
-    window.location.href = "index.html";
+function crearColumnas(trTask,element){
+    
+    Object.keys(element).forEach((propertie => {
+
+        //Creamos el elemento para la columna
+        let tdTask = document.createElement('td');
+        // Le asignamos el valor de la propiedad del objeto literal
+        tdTask.textContent = element[propertie];
+        
+        //Añadimos una checkbox
+        if(element[propertie] === "No completada"){
+            tdTask.textContent = null;
+
+            let input = document.createElement('input');
+            input.type = "checkbox";
+            tdTask.appendChild(input);
+        }
+
+        //Añadimos un botón para eliminar cada tarea
+        if(element[propertie] === null){
+            let button = document.createElement('button');
+            button.textContent = "Eliminar";
+            button.addEventListener('click', eliminarTarea)
+            tdTask.appendChild(button);
+        }
+        //Añadimos el elemento a la fila
+        trTask.appendChild(tdTask);
+
+    }))
+}
+
+function eliminarTarea(){
+
+    null
+
 }
