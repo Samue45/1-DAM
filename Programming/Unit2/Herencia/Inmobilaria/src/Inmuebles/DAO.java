840af0db67094b644afb0fc27df19f7e58f924e6
Samuel <samuelayllonsevilla86@gmail.com>
2025-01-23 09:15:47 +0100

    Ejercicio inmobiliaria

---
+++ Programming/Unit2/Herencia/Inmobilaria/src/Inmuebles/DAO.java
@@ -0,0 +1,37 @@
+package Inmuebles;
+
+import Inmuebles.HijosInmuebles.HijosConstrucciones.Vivienda;
+import Inmuebles.HijosInmuebles.HijosSuperficies.Solar;
+import Inmuebles.HijosInmuebles.HijosSuperficies.Zona;
+import Inmuebles.HijosInmuebles.TiposConstrucciones;
+
+import java.util.ArrayList;
+
+public class DAO {
+
+    private static DAO dao = null;
+    private ArrayList<Inmuebles> lista;
+
+    private DAO(){
+        lista = new ArrayList<>();
+
+        lista.add(new Solar("Jerez",56.78, 0,12.78, Zona.RUSTICA));
+        lista.add(new Vivienda("Cádiz",43,1, TiposConstrucciones.NUEVOS,12,5,2));
+        lista.add(new Solar("Jaén",2.78, 2,23.78, Zona.URBANA));
+        lista.add(new Vivienda("Huelva",20,3, TiposConstrucciones.SEGUNDA_MANO,45,3,1));
+
+
+
+    }
+
+
+
+
+
+
+    //Método para comprobar si existe ya una instancia de la clase DAO
+    public static DAO getInstance(){
+        if (dao == null) dao = new DAO();
+        return dao;
+    }
+}
