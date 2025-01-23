4264dfdbe57fb8c2f6a1ba24741172495042909f
Samuel <136899093+Samue45@users.noreply.github.com>
2025-01-23 17:03:03 +0100

    Mejoras

--- Markup language/Part 2/JS/Exercises/8.1/script.js
+++ Markup language/Part 2/JS/Exercises/8.1/script.js
@@ -47,54 +47,57 @@ function createTask(){
 
 function crearFila(tarea){
 
+    //1º Creamos la fila de la tabla
     let tareaTr = document.createElement('tr');
 
+    //2º Le damos un identifiador a la tabla
+    tareaTr.setAttribute('title',contador);
+
+    // 3º Creamos las columnas de la fila y le asignamos los valores a cada celda
     crearColumna(tareaTr,tarea);
 
+    // Añadimos un estilo por defecto
+    tareaTr.classList.add('incompleta');
+
+    // 4º Finalmente, añadimos la fila a la tabla
     table.appendChild(tareaTr);
 
 }
 
 function crearColumna(trTask,tarea){
 
+    // 1º Recorremos cada tarea que se ha guardado en el array, de modo que podemos generar varias columnas para cada propiedad de tarea
     Object.keys(tarea).forEach((propertie => {
 
+        // 2º No queremos mostrar el identificador que tiene asociada cada tarea de la tabla, por lo que no se crea ninguna columna cuando la propiedad sea el id de la tarea
         if(propertie != 'id'){
-            //Creamos el elemento para la columna
-        let tdTask = document.createElement('td');
-
-        //Obtenemos el id de la tarea
-        let idTarea = tarea.id;
+            
+            // 3º Creamos el elemento para la columna
+            let tdTask = document.createElement('td');
         
-        // Si la propiedad es 'estado', creamos un checkbox en lugar de texto
-         if (propertie === 'estado') {
-         let input = document.createElement('input');
-         input.type = "checkbox";
-         input.checked = tarea[propertie];  // Marca el checkbox si el estado es 'true'
-         input.addEventListener('change', (e) => actualizarEstadoTarea(idTarea, e.target.checked));
-         tdTask.appendChild(input);
-         } else {
-         // Para las otras propiedades, simplemente mostramos su valor
-         tdTask.textContent = tarea[propertie];
-         }
- 
-         //Añadimos un botón para eliminar cada tarea
-         if(tarea[propertie] === null){
-             let button = document.createElement('button');
-             button.textContent = "Eliminar";
-             button.addEventListener('click', () => eliminarTarea(idTarea))
-             tdTask.appendChild(button);
-         }
-         //Añadimos el elemento a la fila
-         trTask.appendChild(tdTask);
- 
-         // Si la tarea está completada, se aplica el estilo correspondiente
-         if (tarea.estado === true) {
-             trTask.classList.add("completada"); // Añadimos una clase CSS para las tareas completadas
-         }
+            // 4º Si la propiedad es 'estado', creamos un checkbox en lugar de texto
+            if (propertie === 'estado') {
+                let input = document.createElement('input');
+                input.type = "checkbox";
+                input.checked = tarea[propertie];  // Marca el checkbox si el estado es 'true'
+                input.addEventListener('change', actualizarEstadoTarea);
+                tdTask.appendChild(input);
+            } else {
+            // 5º Para las otras propiedades, simplemente mostramos su valor
+            tdTask.textContent = tarea[propertie];
+            }
+
+            // 6º Añadimos un botón para eliminar cada tarea
+            if(tarea[propertie] === null){
+                let button = document.createElement('button');
+                button.textContent = "Eliminar";
+                button.addEventListener('click', eliminarTarea)
+                tdTask.appendChild(button);
+            }
+            // 7º Añadimos el elemento a la fila
+            trTask.appendChild(tdTask);
+
         }
-
-        
 
     }))
 
@@ -126,28 +129,37 @@ function crearCabecera(){
 
 
 
-function actualizarEstadoTarea(id, estadoCompletado) {
+function actualizarEstadoTarea(e) {
+   
+    let idTareaAsociada = e.target.parentElement.parentElement.getAttribute('title');
+    let tareaAsociada = arrayTareas.find((tarea) => {return tarea.id == idTareaAsociada});
 
-    arrayTareas.forEach((element, index) =>{
+    tareaAsociada.estado = !tareaAsociada.estado;
 
-        //El cambio se aplica sobre el objeto literal guardado en el Array, pero no sobre el elemento que está en el DOM
-        if(element.id === id) {
-            // Actualizamos el estado de la tarea en el array usando su índice
-            arrayTareas[index].estado = estadoCompletado ? true : false;
-        }
-    })
+    if(tareaAsociada.estado){
+        e.target.parentElement.parentElement.classList.add('completada');
+        e.target.parentElement.parentElement.classList.remove('incompleta');
+         
+    }
+    else{
+        e.target.parentElement.parentElement.classList.add('incompleta');
+        e.target.parentElement.parentElement.classList.remove('completada');
+        
+    }
+
 }
 
-function eliminarTarea(id){
+function eliminarTarea(e){
 
-    arrayTareas.forEach((element, index) =>{
+    // 1º Tenemos que buscar la tarea en el DOM por su id
+    let idTareaAsociada = e.target.parentElement.parentElement.getAttribute('title');
+    // 2º Necesitamos buscar la tarea en el array de tareas por su id
+    let tareaAsociada = arrayTareas.find((tarea) => {return tarea.id == idTareaAsociada});
+    
+    // 3º Como se trata del botón su padre es el elemento td, a su vez el padre del td es el tr y es este el elemento que queremos eliminar
+    e.target.parentElement.parentElement.remove();
 
-
-        //Se elimina del Array pero no del DOM
-        if(element.id == id) {
-             // Eliminar la tarea del array
-            arrayTareas.splice(index, 1);
-        }
-    })
+    // 4º Eliminamos la tarea de la lista de tareas, para ello usamos el método filter y solo guardamos las tareas cuyo id sea diferente de la tarea que hemos eliminado
+    arrayTareas.filter((tarea) => { return tareaAsociada != idTareaAsociada});
 
 }
