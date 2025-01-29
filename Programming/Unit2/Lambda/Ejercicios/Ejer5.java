6e5d5c978394b8015ec46081667b056f0cfc7373
Samuel <samuelayllonsevilla86@gmail.com>
2025-01-29 10:02:32 +0100

    Lambda

---
+++ Programming/Unit2/Lambda/Ejercicios/Ejer5.java
@@ -0,0 +1,7 @@
+package Programming.Unit2.Lambda.Ejercicios;
+
+@FunctionalInterface
+public interface Ejer5 {
+
+    int lessNumber(int[] listaNumbers);
+}
--- Programming/Unit2/Lambda/Ejercicios/Main.java
+++ Programming/Unit2/Lambda/Ejercicios/Main.java
@@ -1,12 +1,13 @@
 package Programming.Unit2.Lambda.Ejercicios;
 
 import java.lang.reflect.Array;
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
 
         //==================================================EJERCICIO 1======================================================
-        int[] arrayNumbers = {1,2,3,4,5,6,7,5};
+        int[] arrayNumbers = {5,2,7,3,4,5,7,76,7,5,7,1,1,1,1,9};
 
         //Expresión lambda
         Ejer1 ejer1 = (a) -> {
@@ -31,7 +32,6 @@ public class Main {
 
         //Clase que implementa la interfaz
         ClaseEjer1 claseEjer1 = new ClaseEjer1();
-        claseEjer1.cacularPromedio(arrayNumbers);
 
         //==================================================EJERCICIO 2======================================================
        //Método abstracto que devuelve el factorial de un número
@@ -46,5 +46,58 @@ public class Main {
         //==================================================EJERCICIO 3======================================================
         //Método abstracto que devuelve si un número es par
         Ejer3 ejer3 = (a) -> (a % 2) == 0;
+
+        //==================================================EJERCICIO 4======================================================
+        //Método abstracto que el número mayor
+        Ejer4 ejer4 = (a) -> {
+            Random random = new Random();
+            int mayor = 0;
+
+            //1º Buscar el mayor y guardarlo
+            for (int number: a){
+                if (number > mayor) mayor= number;
+            }
+            // 2º Retornar el mayor
+            return mayor;
+
+        };
+
+        //==================================================EJERCICIO 5======================================================
+        //Método abstracto que el número menor
+        Ejer5 ejer5 = (a) -> {
+            Random random = new Random();
+            int menor = 0;
+
+            //1º Buscar el mayor y guardarlo
+            for (int number: a){
+                if (number < menor) menor= number;
+            }
+            // 2º Retornar el mayor
+            return menor;
+        };
+
+        //==================================================EJERCICIO 6======================================================
+        //Método abstracto que devuelve el número que se repite más veces
+        Ejer6 ejer6 = (a) -> {
+            int contador= 0;
+            int contadorMayor = 0;
+            int numeroMasRepetido = 0;
+
+            for (int number1 : a){
+                for (int number2 : a){
+                    if (number1 == number2) contador++;
+                    //Guardamos el número de veces que aparece un número
+                    //Comparamos su contador con el del siguiente número y siempre nos quedamos con el mayor y con el número
+                    if (contador > contadorMayor){
+                        contadorMayor = contador;
+                        numeroMasRepetido = number2;
+                    }
+                    //Reiniciamos el contador
+                    contador = 0;
+                }
+            }
+            return numeroMasRepetido;
+        };
+
     }
 }
