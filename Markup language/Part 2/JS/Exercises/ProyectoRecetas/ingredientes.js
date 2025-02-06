a44ab63518f7e490615656e375a5489a087df338
Samuel <xamuelx19@gmail.com>
2025-02-06 10:14:46 +0100

    Recetas

---
+++ Markup language/Part 2/JS/Exercises/ProyectoRecetas/ingredientes.js
@@ -0,0 +1,14 @@
+class Ingredientes {
+
+    constructor(nombre,cantidad, calorias){
+        this.nombre = nombre;
+        this.cantidad = cantidad;
+        this.calorias = calorias
+    }
+
+    
+
+
+}
+
+export default Ingredientes;
---
+++ Markup language/Part 2/JS/Exercises/ProyectoRecetas/receta.js
@@ -0,0 +1,22 @@
+class Receta {
+//Constructor
+constructor (nombre,[ingredientes],descripcion,categoria){
+    this.nombre = nombre;
+    this.ingredientes = [ingredientes];
+    this.descripcion = descripcion;
+    this.categoria = categoria;
+}
+
+//Métodos
+
+
+
+
+
+
+
+
+
+
+
+}
