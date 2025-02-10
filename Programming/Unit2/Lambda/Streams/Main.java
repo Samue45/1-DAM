5952a6d413df543286582d36f6fc10aea6d6b77b
Samuel <136899093+Samue45@users.noreply.github.com>
2025-02-10 18:43:51 +0100

    study

--- Programming/Unit2/Lambda/Streams/Main.java
+++ Programming/Unit2/Lambda/Streams/Main.java
@@ -2,6 +2,7 @@ package Programming.Unit2.Lambda.Streams;
 
 
 import java.util.ArrayList;
+import java.util.Comparator;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,6 +11,7 @@ public class Main {
         ArrayList<Persona> listaAlumnos = crearDatos.crearAlumnos();
         ArrayList<Curso> listaCursos = crearDatos.crearCursos();
 
+        /*
         //1
         long totalAlumnos = listaCursos.stream()
                 .filter((curso) -> curso.getListaAlumnos().size() > 5)
@@ -18,9 +20,22 @@ public class Main {
 
         //2
         System.out.println("----------Lista de títulos----------");
-        listaCursos.stream()
+            listaCursos.stream()
                 .filter(curso -> curso.getCantidadVideos() >= 10 && curso.getCantidadVideos() <= 50)
                 .map(curso -> curso.getTitulo())
                 .forEach(titulo -> System.out.println(titulo));
+
+        //3
+        listaCursos.stream()
+                .filter((curso -> curso.getListaAlumnos().size() > 4))
+                .forEach((curso -> {
+                    System.out.println("Título del curso = " + curso.getTitulo() + "\nDuración del curso = " + curso.getDuracion());
+                }));
+*/
+        //4
+       listaCursos.stream()
+                .min((curso1, curso2) -> Integer.compare(curso1.getListaAlumnos().size(), curso2.getListaAlumnos().size()))
+                .ifPresent((curso -> System.out.println(curso.getTitulo())));
+
     }
 }
