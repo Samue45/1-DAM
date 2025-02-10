f10aed294ca935c9ea9098d11754c6ee61d6263c
Samuel <xamuelx19@gmail.com>
2025-02-10 12:47:03 +0100

    ejercicios

--- Programming/Unit2/Lambda/Streams/Main.java
+++ Programming/Unit2/Lambda/Streams/Main.java
@@ -1,37 +1,26 @@
 package Programming.Unit2.Lambda.Streams;
 
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
-import java.util.stream.Stream;
+
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
 
-        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5);
-        List<String> palabras = Arrays.asList("java", "stream", "example", "pa");
+        CrearDatos crearDatos = new CrearDatos();
+        ArrayList<Persona> listaAlumnos = crearDatos.crearAlumnos();
+        ArrayList<Curso> listaCursos = crearDatos.crearCursos();
 
         //1
-        int integerStream = numeros.stream()
-                .filter(number -> number % 2 == 0)
-                .reduce(0, (number1, number2) -> number1 + number2);
-
-        System.out.println( "Suma de números pares = " + integerStream);
+        long totalAlumnos = listaCursos.stream()
+                .filter((curso) -> curso.getListaAlumnos().size() > 5)
+                .count();
+        System.out.println("Cantidad de cursos que tienen más de 300 alumnos: " + totalAlumnos);
 
         //2
-        int integerStream1 = numeros.stream().max(Comparator.naturalOrder()).orElseThrow();
-        System.out.println("El número más grande = " + integerStream1);
-
-        //3
-        List<String> nameToUpperCase = palabras.stream().map( palabra -> palabra.toUpperCase()).toList();
-        System.out.println("Nombres a mayúscula = " + nameToUpperCase);
-
-        //4
-        long numbers3 = numeros.stream().limit(3).count();
-        System.out.println("3 primeros números de la lista = " + numbers3);
-
-        //5
-        List<String> palabrasLargas = palabras.stream().filter(palabra -> palabra.length() > 3).toList();
-        System.out.println("Palabras con más de 3 letras = " + palabrasLargas);
+        System.out.println("----------Lista de títulos----------");
+        listaCursos.stream()
+                .filter(curso -> curso.getCantidadVideos() >= 10 && curso.getCantidadVideos() <= 50)
+                .map(curso -> curso.getTitulo())
+                .forEach(titulo -> System.out.println(titulo));
     }
 }
