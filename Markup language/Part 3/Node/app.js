723c29b84a1008f321affac1072ce00c71094f6d
Samuel <xamuelx19@gmail.com>
2025-02-19 13:12:33 +0100

    cambios

--- Markup language/Part 3/Node/app.js
+++ Markup language/Part 3/Node/app.js
@@ -1,5 +1,19 @@
-let a = 3;
+// Importar express
+const express = require('express');
 
-if(a == 3){
-    console.log("hola");
-}
+// Crear una aplicación express
+const app = express();
+
+// Definir un puerto para el servidor
+const port = 3000;
+
+// Ruta básica de prueba
+app.get('/', (req, res) => {
+  res.send('¡Hola, mundo!');
+});
+
+// Iniciar el servidor
+app.listen(port, () => {
+  console.log(`Servidor corriendo en http://localhost:${port}`);
+});
+
