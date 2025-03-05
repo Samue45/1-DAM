622aecce1811af8bcaf8d2a8a6e06aaff7d52928
Samuel <xamuelx19@gmail.com>
2025-03-05 14:11:48 +0100

    proyecto

---
+++ MasterMind/src/ServicioJuego.js
@@ -0,0 +1,30 @@
+export class Servicio {
+
+
+
+    //Datos
+    // Array que contiene la combinación de números de la solución
+    /*Cada color es representado por un número*/ 
+
+    
+
+
+    //Métodos
+    /*
+    1- void crearSolucion()
+    2- array comprobarRespuesta(Array  respuesta del usuario) {
+
+
+        // Array de pistas
+        /*
+        Hay 3 posibles pistas, cada pista está representada por un número
+
+        -
+    
+        */ 
+
+    
+
+}
+
+
