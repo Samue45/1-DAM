bf251eeed8fd153eae718f53a0b5ce81b3914b5a
samuelayllonsevilla86@gmail.com <samuelayllonsevilla86@gmail.com>
2025-03-14 13:58:59 +0100

    practica

--- Markup language/Part 4/ejercicio1.js
+++ Markup language/Part 4/ejercicio1.js
@@ -1,14 +1,7 @@
 // 1º Crear una función esperarTiempo(ms) devuelve una promesa resuelta
 
-function esperarTiempo(ms){
-    setTimeout(() => {
-        return fetch('https://api.ejemplo.com/submit')
-        .then(response => {})
-        .catch(error => {})
-    }, ms);
-    
-    
-}
-
 // 2º Manejar la promesa con  .then().catch()
+
+
+
 // 3º Manejar la promesa con async/await y try...catch.
